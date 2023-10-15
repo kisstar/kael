6a9e0c6728a247286170265eecf5a5e3f58ae48d
kisstar <dwh.chn@foxmail.com>
2023-10-15 17:49:07 +0800

    feat(kael,kael-logger): configuring the logger through environment variables

--- packages/core/kael/src/main.rs
+++ packages/core/kael/src/main.rs
@@ -2,6 +2,8 @@ mod cli;
 mod exec;
 
 fn main() {
+    kael_logger::init();
+
     let matches = cli::create_commander().get_matches();
 
     exec::run(matches);
--- packages/shared/kael-logger/src/lib.rs
+++ packages/shared/kael-logger/src/lib.rs
@@ -1,1 +1,9 @@
+use env_logger::Builder;
+
 pub use log::{debug, error, info, warn};
+
+pub fn init() {
+    let mut builder = Builder::from_env(env_logger::Env::default().default_filter_or("info"));
+
+    builder.init();
+}
